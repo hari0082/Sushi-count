668ed82986ddd94075623e125ef483a5d3b0c23d
hari0082 <2430680@rts-365.dk>
2025-02-24 15:42:34 +0100

    add all

---
+++ sushi-counter/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const addSushi = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div className="App">
+      <div className="sushi-background"></div>
+      <h1 className="title">Sushi Tæller</h1>
+      <p className="count">Sushi: {count}</p>
+      <button className="add-button" onClick={addSushi}>
+        Tilføj
+      </button>
+    </div>
+  );
+}
+
+export default App;
